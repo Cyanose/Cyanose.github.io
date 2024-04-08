d42d7a1b343372aea58c4c4b0ba9ef045367c243
Cyanose <KrzysztofPastuszka@protonmail.com>
2024-04-08 14:10:15 +0200

    just hide 2 subtabts

--- src_js/table.js
+++ src_js/table.js
@@ -1,6 +1,6 @@
 function showTables(buttonNumber) {
   // Hide all tables
-  for (let i = 1; i <= 8; i++) {
+  for (let i = 1; i <= 4; i++) {
     document.getElementById(`table${i}`).classList.add("hidden");
   }
 
@@ -11,11 +11,11 @@ function showTables(buttonNumber) {
   } else if (buttonNumber === 2) {
     document.getElementById("table3").classList.remove("hidden");
     document.getElementById("table4").classList.remove("hidden");
-  } else if (buttonNumber === 3) {
-    document.getElementById("table5").classList.remove("hidden");
-    document.getElementById("table6").classList.remove("hidden");
-  } else if (buttonNumber === 4) {
-    document.getElementById("table7").classList.remove("hidden");
-    document.getElementById("table8").classList.remove("hidden");
+  // } else if (buttonNumber === 3) {
+  //   document.getElementById("table5").classList.remove("hidden");
+  //   document.getElementById("table6").classList.remove("hidden");
+  // } else if (buttonNumber === 4) {
+  //   document.getElementById("table7").classList.remove("hidden");
+  //   document.getElementById("table8").classList.remove("hidden");
   }
 }
